3eacad5b640603f41450b625fc102619d7e2b705
Lauro Jacques - DTI <lauro.jacques@dtidigital.com.br>
2018-05-17 12:02:14 -0300

    Servidor rodando e pagina inicial tbm

--- server/app.js
+++ server/app.js
@@ -1,15 +1,18 @@
 var express = require('express'),
     app = express();
+const fs = require('fs');
+const url = require('url');
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
 var db = {
+    jogadores: JSON.parse(fs.readFileSync('server/data/jogadores.json', 'utf8')),
+    jogosPorJogador: JSON.parse(fs.readFileSync('server/data/jogosPorJogador.json', 'utf8'))
 };
 
-
 // configurar qual templating engine usar. Sugestão: hbs (handlebars)
-//app.set('view engine', '???');
+app.set('view engine', 'hbs');
 
 
 // EXERCÍCIO 2
@@ -17,7 +20,10 @@ var db = {
 // dados do banco de dados "data/jogadores.json" com a lista de jogadores
 // dica: o handler desta função é bem simples - basta passar para o template
 //       os dados do arquivo data/jogadores.json
-
+app.set('views', 'server/views');
+app.get('/', function (req, res) {
+    res.render('index', db);
+});
 // EXERCÍCIO 3
 // definir rota para página de detalhes de um jogador --> renderizar a view
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
@@ -28,6 +34,8 @@ var db = {
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
 // dica: 1 linha de código
+app.use(express.static('client'));
 
 // abrir servidor na porta 3000
 // dica: 1-3 linhas de código
+app.listen(3000);
