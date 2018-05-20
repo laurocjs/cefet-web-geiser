1c7c3f25d9336d6d195e8f859da80f5abe6f0f4c
Lauro Jacques - DTI <lauro.jacques@dtidigital.com.br>
2018-05-19 23:55:17 -0300

    Pagina do jogador criada

--- server/app.js
+++ server/app.js
@@ -3,6 +3,10 @@ var express = require('express'),
 const fs = require('fs');
 const url = require('url');
 
+function funcDeComparacaoDeJogos(a, b) {
+    return -(a.playtime_forever - b.playtime_forever);
+}
+
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
@@ -29,7 +33,33 @@ app.get('/', function (req, res) {
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter umas 15 linhas de código
+app.get('/jogador/:numero_identificador/', function (req, res) {
+    for (let player of db.jogadores.players) {
+        if (player.steamid == req.params.numero_identificador) {
+            let dadosjogador = {};
+            dadosjogador.jogador = player;
+            let jogos = db.jogosPorJogador[player.steamid];
+            dadosjogador.jogador.numerodejogos = jogos.game_count;
+            dadosjogador.jogador.numerodejogosnaojogados = 0;
+            dadosjogador.jogador.listadejogos = [];
+            for (let jogo of jogos.games) {
+                if (jogo.playtime_forever > 40) { // só considerei jogado se jogou pelo menos 40m
+                    jogo.tempo = (jogo.playtime_forever/60).toFixed();
+                    dadosjogador.jogador.listadejogos.push(jogo);
+                }
+                else {
+                    dadosjogador.jogador.numerodejogosnaojogados++;
+                }
+            }
 
+            dadosjogador.jogador.listadejogos.sort(funcDeComparacaoDeJogos);
+            dadosjogador.jogador.listadejogos.splice(5, dadosjogador.jogador.listadejogos.length);
+            dadosjogador.jogador.jogopreferido = dadosjogador.jogador.listadejogos[0];          
+            res.render('jogador', dadosjogador);
+        }
+    }
+    db.idDoJogador = req.params.numero_identificador;
+});
 
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
